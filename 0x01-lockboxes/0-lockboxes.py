d701bf19869fb6e8547e5cfe14b49cc846b8fa24
khaledolemy <khaledolemy@gmail.com>
2024-06-07 01:37:29 +0300

    f

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -29,8 +29,8 @@ def singleBoxUnlocker(boxes, keyToBox, unlockedBoxes=[0]):
         return True
     if not boxes[keyToBox]:
         return False
-    # if (len(boxes[keyToBox]) == 1 and keyToBox == boxes[keyToBox][0]):
-    #     return False
+    if (len(boxes[keyToBox]) == 1 and keyToBox == boxes[keyToBox][0]):
+        return False
     for b in boxes[keyToBox]:
         if b != keyToBox:
             if b not in unlockedBoxes:
