5ec256b6678c86bb7103af11fd70c2393db1067e
khaledolemy <khaledolemy@gmail.com>
2024-06-13 21:32:26 +0300

    problem solution

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Min Operations
+"""
+
+
+def minOperations(n: int) -> int:
+    """
+    Min Operations
+    """
+    currentCount: int = 1  # initial H count
+    copier: int = 1  # initial copier of the H (current count) in the file
+    noOperations: int = 1  # Initial copy (first operation)
+    while (n > currentCount):
+        remainingCount = n - currentCount
+        if remainingCount / (copier * 2) == remainingCount // (copier * 2):
+            copier = copier * 2
+            noOperations += 2  # Paste then copy
+        else:
+            noOperations += 1  # Paste
+        currentCount += copier
+    return noOperations
