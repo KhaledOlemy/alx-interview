108defaeb4d0cf2f8919516019674c0548620c63
khaledolemy <khaledolemy@gmail.com>
2024-06-13 21:36:06 +0300

    remove inline comments

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -8,15 +8,15 @@ def minOperations(n: int) -> int:
     """
     Min Operations - H copier
     """
-    currentCount: int = 1  # initial H count
-    copier: int = 1  # initial copier of the H (current count) in the file
-    noOperations: int = 1  # Initial copy (first operation)
+    currentCount: int = 1
+    copier: int = 1
+    noOperations: int = 1
     while (n > currentCount):
         remainingCount = n - currentCount
         if remainingCount / (copier * 2) == remainingCount // (copier * 2):
             copier = copier * 2
-            noOperations += 2  # Paste then copy
+            noOperations += 2
         else:
-            noOperations += 1  # Paste
+            noOperations += 1
         currentCount += copier
     return noOperations
