6b4cb7fecc115eb78e4d68e9883ab3ac7fc6d955
khaledolemy <khaledolemy@gmail.com>
2024-07-29 08:50:21 +0300

    Island Perimeter problem solution

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Solve island perimeter problem
+Get the surrounding water tiles next to
+land tiles"""
+from typing import List
+
+
+def island_perimeter(grid: List) -> int:
+    perimeter = 0
+    for i, row in enumerate(grid):
+        for j, col in enumerate(row):
+            if col == 1:
+                continue
+            to_check = []
+            if j != len(row) - 1:
+                to_check.append(row[j+1])
+            if i != len(grid) - 1:
+                to_check.append(grid[i+1][j])
+            if j != 0:
+                to_check.append(row[j-1])
+            if i != 0:
+                to_check.append(grid[i-1][j])
+            perimeter += sum(to_check)
+    return perimeter
